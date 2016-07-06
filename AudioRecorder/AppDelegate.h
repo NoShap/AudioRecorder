de1123f198b4e8546e9e732502a13d2731d01689
Noah Shapiro <noahs921@gmail.com>
2016-07-06 11:55:59 -0400

    Initial Commit

---
+++ AudioRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioRecorder
+//
+//  Created by Noah Shapiro on 7/6/16.
+//  Copyright © 2016 Noah Shapiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioRecorder
+//
+//  Created by Noah Shapiro on 7/6/16.
+//  Copyright © 2016 Noah Shapiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
